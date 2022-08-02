d8ba2c0b2f0cf9908a35fa49f2cd9cda727e227e
Karthick2299 <106722215+Karthick2299@users.noreply.github.com>
2022-08-02 15:24:24 +0530

    Basic counter Project added

---
+++ CounterProject/js/counter.js
@@ -0,0 +1,23 @@
+const showNumber = document.getElementById("show-number");
+
+let count = 0;
+
+showNumber.innerHTML = 0;
+
+ function increase(){
+     let increment = ++count;
+     showNumber.innerHTML = increment;
+  
+    
+}
+
+ function decrease(){
+  
+    let decrement = --count;
+    showNumber.innerHTML = decrement;
+}
+
+function reset(){
+    count = 0;
+    showNumber.innerHTML = 0;
+}
