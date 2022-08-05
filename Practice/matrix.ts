0ebbd3e637aec7c050249a492fd1281b003cce53
Karthick2299 <106722215+Karthick2299@users.noreply.github.com>
2022-08-05 18:43:39 +0530

    mine sweeper project added

---
+++ Practice/matrix.ts
@@ -0,0 +1,31 @@
+//for practicing multi dimensional array in ts
+
+// var mat:number[][] = [[1,2,3], [4,5,6]];
+// console.log(mat);
+// console.log(mat[0][2]);
+
+//get input value from User
+var bodyElement = <HTMLBodyElement>document.getElementById("app");
+var row = <HTMLInputElement>document.getElementById("row");
+
+var column = <HTMLInputElement>document.getElementById("col");
+
+function view() {
+    let rowValue = parseInt(row?.value);
+    console.log(rowValue);
+    let colValue = parseInt(column?.value);
+    console.log(colValue);
+
+    
+    for(let i = 0; i < rowValue;i++ ){
+        // console.log("its working");
+      
+        for(let j = 0; j < colValue; j++){
+            const divElement = document.createElement("div");
+            divElement.classList.add("divStyle");
+            bodyElement.append(divElement);
+            console.log(divElement);
+        }
+    
+    }
+}
